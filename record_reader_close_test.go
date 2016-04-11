2980819f2d46940eb27e41d4eec2eac1af7ef74f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2016-04-11 07:37:52 -0700

    added more tests

--- record_reader_close_test.go
+++ record_reader_close_test.go
@@ -12,13 +12,99 @@ import (
 
 func TestRecordReaderClose(t *testing.T) {
 
-	reader := ioutil.NopCloser( strings.NewReader("apple banana cherry") )
+	tests := []struct{
+		String string
+	}{
+		{
+			String: "",
+		},
+		{
+			String: "\x1f",
+		},
 
-        rr := NewRecordReader(reader)
 
-	if err := rr.Close(); nil != err {
-		t.Errorf("Did not expect an error but actually got one: %v", err)
-		return
+
+		{
+			String: "\x1e",
+		},
+		{
+			String: "\x1e\x1f",
+		},
+
+
+
+		{
+			String: "\x1e\x1e",
+		},
+		{
+			String: "\x1e\x1e\x1f",
+		},
+
+
+
+		{
+			String: "\x1e\x1e\x1f\x1e\x1e\x1f\x1e\x1e",
+		},
+		{
+			String: "\x1e\x1e\x1f\x1e\x1e\x1f\x1e\x1e\x1f",
+		},
+
+
+
+		{
+			String: "apple banana cherry",
+		},
+		{
+			String: "apple banana cherry\x1f",
+		},
+
+
+
+		{
+			String: "fruit\x1fdruit",
+		},
+		{
+			String: "fruit\x1fdruit\x1f",
+		},
+
+
+
+		{
+			String: "fruit\x1fapple\x1fbanana\x1fcherry",
+		},
+		{
+			String: "fruit\x1fapple\x1fbanana\x1fcherry\x1f",
+		},
+
+
+
+		{
+			String: "apple\x1ebanana\x1echerry\x1fapple\x1ebanana\x1echerry",
+		},
+		{
+			String: "apple\x1ebanana\x1echerry\x1fapple\x1ebanana\x1echerry\x1f",
+		},
+
+
+
+		{
+			String: "given_name\x1efamily_name\x1ecity\x1fJoe\x1eBlow\x1eVancouver\x1fJane\x1eDoe\x1eSurrey",
+		},
+		{
+			String: "given_name\x1efamily_name\x1ecity\x1fJoe\x1eBlow\x1eVancouver\x1fJane\x1eDoe\x1eSurrey\x1f",
+		},
+	}
+
+
+	for testNumber, test := range tests {
+		reader := ioutil.NopCloser( strings.NewReader(test.String) )
+
+	        rr := NewRecordReader(reader)
+
+		if err := rr.Close(); nil != err {
+			t.Errorf("For test #%d, did not expect an error but actually got one: %v", testNumber, err)
+			return
+		}
 	}
 }
 
@@ -26,15 +112,102 @@ func TestRecordReaderClose(t *testing.T) {
 
 func TestRecordReaderCloseThenNext(t *testing.T) {
 
-	reader := ioutil.NopCloser( strings.NewReader("apple\x1ebanana\x1echerry\x1fapple\x1ebanana\x1echerry\x1f") )
+	tests := []struct{
+		String string
+	}{
+		{
+			String: "",
+		},
+		{
+			String: "\x1f",
+		},
 
-        rr := NewRecordReader(reader)
 
-	rr.MustClose()
 
-	next := rr.Next()
-	if expected, actual := false, next; expected != actual {
-		t.Errorf("Expected %t but actually got %t.", expected, actual)
-		return
+		{
+			String: "\x1e",
+		},
+		{
+			String: "\x1e\x1f",
+		},
+
+
+
+		{
+			String: "\x1e\x1e",
+		},
+		{
+			String: "\x1e\x1e\x1f",
+		},
+
+
+
+		{
+			String: "\x1e\x1e\x1f\x1e\x1e\x1f\x1e\x1e",
+		},
+		{
+			String: "\x1e\x1e\x1f\x1e\x1e\x1f\x1e\x1e\x1f",
+		},
+
+
+
+		{
+			String: "apple banana cherry",
+		},
+		{
+			String: "apple banana cherry\x1f",
+		},
+
+
+
+		{
+			String: "fruit\x1fdruit",
+		},
+		{
+			String: "fruit\x1fdruit\x1f",
+		},
+
+
+
+		{
+			String: "fruit\x1fapple\x1fbanana\x1fcherry",
+		},
+		{
+			String: "fruit\x1fapple\x1fbanana\x1fcherry\x1f",
+		},
+
+
+
+		{
+			String: "apple\x1ebanana\x1echerry\x1fapple\x1ebanana\x1echerry",
+		},
+		{
+			String: "apple\x1ebanana\x1echerry\x1fapple\x1ebanana\x1echerry\x1f",
+		},
+
+
+
+		{
+			String: "given_name\x1efamily_name\x1ecity\x1fJoe\x1eBlow\x1eVancouver\x1fJane\x1eDoe\x1eSurrey",
+		},
+		{
+			String: "given_name\x1efamily_name\x1ecity\x1fJoe\x1eBlow\x1eVancouver\x1fJane\x1eDoe\x1eSurrey\x1f",
+		},
+	}
+
+
+	for testNumber, test := range tests {
+
+		reader := ioutil.NopCloser( strings.NewReader(test.String) )
+
+	        rr := NewRecordReader(reader)
+
+		rr.MustClose()
+
+		next := rr.Next()
+		if expected, actual := false, next; expected != actual {
+			t.Errorf("For test %#d, expected %t but actually got %t.", testNumber, expected, actual)
+			return
+		}
 	}
 }
